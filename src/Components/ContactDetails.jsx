5e87666e034f278b4c1e3129d48821b9c7f45b01
root <root@DESKTOP-E6801AM>
2023-07-06 19:29:41 -0400

    Initial commit

---
+++ src/Components/ContactDetails.jsx
@@ -0,0 +1,7 @@
+const ContactDetails = ({ details }) => (
+    <div>
+        <h1>Username: {details.address}</h1>
+    </div>
+)
+
+export default ContactDetails;
---
+++ src/Components/ContactRow.jsx
@@ -0,0 +1,14 @@
+    const ContactRow = ({ contact, setSelectedContactId, setContactDetails }) => {
+    return(
+    <tr onClick={() => { 
+        setSelectedContactId(contact.id)
+        setContactDetails(contact)
+        }}>
+        <td>{contact.name}</td>
+        <td>{contact.email}</td>
+        <td>{contact.phone}</td>
+    </tr>
+    )
+}
+
+export default ContactRow;
